3bbb925a80825f0dc563c107f7fa68cc92b8b6e1
yoyo46 <muhammad.iksan@live.com>
2014-12-04 22:20:16 +0700

    Template

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Nb27EvmwAaqGyytf3LMvxWKLF2YUcCjVecdKafSeRPKfdPnG');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '4856976934523125464859563452346368');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
