cdcdd4bd61af05bb1cdb6de0a8b6ae6d17dfad6a
yoyo46 <muhammad.iksan@live.com>
2014-12-04 22:20:28 +0700

    Template

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -45,32 +45,9 @@ class PagesController extends AppController {
  *	or MissingViewException in debug mode.
  */
 	public function display() {
-		$path = func_get_args();
-
-		$count = count($path);
-		if (!$count) {
-			return $this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
-
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-
-		try {
-			$this->render(implode('/', $path));
-		} catch (MissingViewException $e) {
-			if (Configure::read('debug')) {
-				throw $e;
-			}
-			throw new NotFoundException();
-		}
+		$this->set('title_for_layout', __('ERP RJTM | Dashboard'));
+		$this->set('module_title', __('Dashboard'));
+		$this->set('sub_module_title', __('Control panel'));
+		$this->render('home');
 	}
 }
