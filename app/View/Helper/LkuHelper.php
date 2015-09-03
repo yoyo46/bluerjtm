8f93cdd929d01dfe8841678e7c15d4fd448f1bf8
yoyo46 <muhammad.iksan@live.com>
2015-09-03 08:42:30 +0700

    [URGENT] LKU / KSU

--- app/View/Helper/LkuHelper.php
+++ app/View/Helper/LkuHelper.php
@@ -12,30 +12,14 @@ class LkuHelper extends AppHelper {
         $completed = $this->Common->filterEmptyField($data, $modelName, 'completed');
         $customStatus = '-';
 
-        if(!empty($completed)){
+        if( !empty($completed) || !empty($complete_paid) ) {
             $customStatus = $this->Html->tag('span', __('Selesai'), array(
                 'class' => 'label label-success',
             ));
-        } else if(!empty($paid)){
-            if(!empty($complete_paid)){
-                $customStatus = $this->Html->tag('span', __('Lunas'), array(
-                    'class' => 'label label-success',
-                ));
-            }else{
-                $customStatus = $this->Html->tag('span', __('Dibayar Sebagian'), array(
-                    'class' => 'label label-info',
-                ));
-            }
-        } else{
-            if(!empty($status)){
-                $customStatus = $this->Html->tag('span', __('Pending'), array(
-                    'class' => 'label label-default',
-                ));
-            } else{
-                $customStatus = $this->Html->tag('span', __('Non-aktif'), array(
-                    'class' => 'label label-danger',
-                ));
-            }
+        } else {
+            $customStatus = $this->Html->tag('span', __('Belum'), array(
+                'class' => 'label label-default',
+            ));
         }
 
         return $customStatus;
