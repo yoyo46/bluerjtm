89805a1a47187da82aed64d970374ee7da7acaff
yoyo46 <muhammad.iksan@live.com>
2015-12-03 22:46:04 +0700

    Format excel & pdf Laporan ledger & Laporan ledger by no doc

--- app/Model/Journal.php
+++ app/Model/Journal.php
@@ -168,7 +168,7 @@ class Journal extends AppModel {
                 $default_options['conditions'] = array_merge($default_options['conditions'], $options['conditions']);
             }
             if(!empty($options['order'])){
-                $default_options['order'] = array_merge($default_options['order'], $options['order']);
+                $default_options['order'] = $options['order'];
             }
             if( isset($options['contain']) && empty($options['contain']) ) {
                 $default_options['contain'] = false;
